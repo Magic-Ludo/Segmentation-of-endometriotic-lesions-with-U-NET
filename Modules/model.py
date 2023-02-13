1082e2596922b81ddb37f5b79e8cacc95521eb24
lucorcos <ludovic.corcos@etu.uca.fr>
2023-02-13 12:37:35 +0100

    Change loss funcion in Training

--- Modules/model.py
+++ Modules/model.py
@@ -6,8 +6,8 @@ def jacard_coef(y_true, y_pred):
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
-    print("-----", intersection, "-----")
-    print("-----", K.sum(y_true_f) + K.sum(y_pred_f) - intersection + 1.0, "-----")
+    # print("-----", intersection, "-----")
+    # print("-----", K.sum(y_true_f) + K.sum(y_pred_f) - intersection + 1.0, "-----")
     return (intersection + 1.0) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + 1.0)
 
 
